5c2fc6c2fb28efbef67e8caae3958847736e44fd
andrew <90sidort@gmail.com>
2020-12-27 16:21:00 +0100

    add delete and rework add comment

--- src/places/components/PlaceDetails.js
+++ src/places/components/PlaceDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 import MapboxGLMap from "../../shared/components/UIElements/Map";
@@ -8,12 +8,16 @@ import { useForm } from "../../shared/hooks/form-hook";
 import { useHttp } from "../../shared/hooks/http-hook";
 import { VALIDATOR_MINLENGTH } from "../../shared/utils/validators";
 import { AuthContext } from "../../shared/context/auth-context";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHistory } from "react-router-dom";
 
 const PlaceDetails = (props) => {
+  const history = useHistory();
   const location = useLocation();
-  const data = { ...location.placeData };
+  const [data, setData] = useState({ ...location.placeData });
   const auth = useContext(AuthContext);
-
+  const [comments, setComments] = useState(null);
   const [formState, inputHandler] = useForm(
     {
       comment: { value: "", isValid: false },
@@ -22,6 +26,19 @@ const PlaceDetails = (props) => {
   );
   const { isLoading, isError, sendRequest, clearError } = useHttp();
 
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${process.env.REACT_APP_BACKEND_URL}/api/places/comments/${data.id}`
+        );
+        console.log(responseData);
+        setComments(responseData.comments);
+      } catch (e) {}
+    };
+    fetchComments();
+  }, [setComments]);
+
   const addCommentHandler = async (e) => {
     e.preventDefault();
     try {
@@ -29,7 +46,7 @@ const PlaceDetails = (props) => {
         comment: formState.inputs.comment.value,
         userId: auth.userId,
       });
-      await sendRequest(
+      const res = await sendRequest(
         `${process.env.REACT_APP_BACKEND_URL}/api/places/comments/${data.id}`,
         "POST",
         body,
@@ -38,58 +55,95 @@ const PlaceDetails = (props) => {
           "Content-Type": "application/json",
         }
       );
+      const resArray = Object.values(res);
+      setComments(resArray);
+      setData(data);
     } catch (e) {
       console.log(e);
     }
   };
 
-  console.log(data);
+  const delCommentHandler = async (commentId) => {
+    const body = JSON.stringify({
+      cid: commentId,
+    });
+    try {
+      const res = await sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/api/places/comments/${data.id}`,
+        "PATCH",
+        body,
+        {
+          Authorization: `Bearer ${auth.token}`,
+          "Content-Type": "application/json",
+        }
+      );
+      const resArray = Object.values(res);
+      setComments(resArray);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  console.log(comments);
 
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <h3>{data.about}</h3>
-      <p>
-        This place has {data.likes} {data.likes === 1 ? "like" : "likes"}
-      </p>
+    <React.Fragment>
+      <ErrorModal error={isError} onClear={clearError} />
+      {isLoading && <LoadingSpinner asOverlay />}
       <div>
-        {" "}
-        <img
-          src={
-            props.image !== "placeholder"
-              ? `${process.env.REACT_APP_BACKEND_URL}/${data.image}`
-              : `${process.env.REACT_APP_BACKEND_URL}/uploads/images/tundra.jpg`
-          }
-          alt={data.title}
-        />
+        <h1>{data.title}</h1>
+        <h3>{data.about}</h3>
+        <p>
+          This place has {data.likes} {data.likes === 1 ? "like" : "likes"}
+        </p>
+        <div>
+          {" "}
+          <img
+            src={
+              props.image !== "placeholder"
+                ? `${process.env.REACT_APP_BACKEND_URL}/${data.image}`
+                : `${process.env.REACT_APP_BACKEND_URL}/uploads/images/tundra.jpg`
+            }
+            alt={data.title}
+          />
+        </div>
+        <div className="map-container">
+          <MapboxGLMap
+            coordinates={[data.coordinates.lat, data.coordinates.lng]}
+          />
+        </div>
+        <p>{data.desc}</p>
+        <form className="place-form" onSubmit={addCommentHandler}>
+          <Input
+            id="comment"
+            type="text"
+            label="Comment"
+            element="input"
+            validators={[VALIDATOR_MINLENGTH(1)]}
+            errorText="Comment must have at least one character."
+            onInput={inputHandler}
+          />
+          <Button disabled={!formState.isValid} type="submit">
+            Add comment
+          </Button>
+        </form>
+        {console.log(123, comments)}
+        {comments && (
+          <div>
+            {comments.map((comment) => {
+              return (
+                <div key={comment._id}>
+                  <p>{comment.text}</p>
+                  <button onClick={() => delCommentHandler(comment._id)}>
+                    X
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
-      <div className="map-container">
-        <MapboxGLMap
-          coordinates={[data.coordinates.lat, data.coordinates.lng]}
-        />
-      </div>
-      <p>{data.desc}</p>
-      <form className="place-form" onSubmit={addCommentHandler}>
-        <Input
-          id="comment"
-          type="text"
-          label="Comment"
-          element="input"
-          validators={[VALIDATOR_MINLENGTH(1)]}
-          errorText="Comment must have at least one character."
-          onInput={inputHandler}
-        />
-        <Button disabled={!formState.isValid} type="submit">
-          Add comment
-        </Button>
-      </form>
-      <div>
-        {data.comments &&
-          data.comments.map((comment) => {
-            return <p>{comment.text}</p>;
-          })}
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
